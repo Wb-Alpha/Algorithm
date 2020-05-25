91e71c14e5736be1307db3c8b03841ab91f4341c
Wb-Alpha <45552873+Wb-Alpha@users.noreply.github.com>
2020-05-25 21:06:33 +0800

    BasicAlgorithmAndTrain

---
+++ DynamicsProgramming/src/Fibonacci/Violent_recursion.java
@@ -0,0 +1,24 @@
+package Fibonacci;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Violent_recursion {
+
+	//≤‚ ‘
+	public static void main(String[] args) {
+		System.out.println("«Î ‰»Î ˝◊÷");
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		System.out.println(fib(N));
+	}
+	
+	
+	//±©¡¶µ›πÈ
+	public static int fib(int N) {
+		if(N==1 || N==2) return 1;
+		return fib(N-1) + fib(N-2);
+	}
+}
+
+
