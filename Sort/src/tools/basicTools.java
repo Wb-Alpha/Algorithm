a351409e896370e83eec0ca948202c6c2e00bbdd
Wb-Alpha <45552873+Wb-Alpha@users.noreply.github.com>
2020-05-25 21:42:20 +0800

    Add fucking-algorithm

--- Sort/src/tools/basicTools.java
+++ Sort/src/tools/basicTools.java
@@ -1,5 +1,16 @@
 package tools;
 
 public class basicTools {
-
+	public static void exch(Comparable[] a, int i, int j) {
+		Comparable t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+	
+	private static void main(Comparable[] a) {
+		for (int i=0; i<a.length; i++) {
+			System.out.print(a[i]+" ");
+			System.out.println();
+		}
+	}
 }
