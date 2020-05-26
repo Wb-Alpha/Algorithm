d63ade87ab9f506283d3cbfee0c876afdbf381ad
Wb-Alpha <45552873+Wb-Alpha@users.noreply.github.com>
2020-05-27 00:33:40 +0800

    DynamicsProgramming
    Update some about CoinsProblam

--- DynamicsProgramming/src/Example/Coin.java
+++ DynamicsProgramming/src/Example/Coin.java
@@ -1,23 +1,35 @@
 package Example;
 
+import java.util.Scanner;
+
 public class Coin {
 	public static void main(String[] args) {
-		
+		Scanner in = new Scanner(System.in);
+		int[] coins;
+		System.out.println("请输入硬币面值");
+		int N = in.nextInt();
+		coins = new int[N];
+		System.out.println("请输入总值");
+		int amount = in.nextInt();
+		in.close();
+		System.out.println(coinChange(coins, amount));
 	}
+
 	
-	public static int coinChange(int[] coins, int amount, int n) {
+	public static int coinChange(int[] coins, int n) {
+		//base case
 		if (n == 0) return 0;
 		if (n < 0) return -1;//无解
 		//求最小值，所以初始化正无穷
-		float res = Float.MAX_VALUE;
+		int res = Integer.MAX_VALUE;
 		for (int coin : coins){
-			
+			int subproblem = coinChange(coins, n-coin);
+			if (subproblem == -1) continue;
+			res = (res < subproblem+1 ? res:subproblem+1);
 		}
-		return 0;
-	}
-	
-	public static void dp(int n) {
-		//base case
-
+		 if (res != Integer.MAX_VALUE)
+			 return res;
+		 else
+			 return -1;
 	}
 }
---
+++ DynamicsProgramming/src/Example/CoinWithNote.java
@@ -0,0 +1,41 @@
+package Example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CoinWithNote {
+	
+	public static void main(String[] args) {
+		Map memo = new HashMap();
+		int[] coins = {1,2,5};
+		int amount = 11;
+		System.out.println(coinChange(memo, coins, amount));
+		
+	}
+	
+	public static int coinChange(Map memo, int[] coins, int amount) {
+		if (memo.containsKey(amount)) return (int) memo.get(amount);
+		
+		//--------------≤‚ ‘÷–--------------
+		/*for(Object key : memo.keySet()){
+		    int value = (int) memo.get(key);
+		    System.out.println(key+":"+value);
+		}*/
+		System.out.println(amount);
+		
+		if (amount == 0) return 0;
+		if (amount < 0) return -1;
+		int res = Integer.MAX_VALUE;
+		for (int coin : coins) {
+			int sub = coinChange(memo, coins, amount - coin);
+			if (sub == -1) continue;
+			res = (res < sub-1? res:sub-1);
+		}
+		
+		if (res != Integer.MAX_VALUE)
+			memo.put(amount, res);
+		else
+			memo.put(amount, -1);
+		return (int)memo.get(amount);
+	}
+}
