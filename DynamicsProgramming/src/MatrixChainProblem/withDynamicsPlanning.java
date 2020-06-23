6b6c6b11db79813d3efc52f2fdabda0ab3af5e2f
Wb-Alpha <45552873+Wb-Alpha@users.noreply.github.com>
2020-06-24 01:51:22 +0800

    NEW

--- DynamicsProgramming/src/MatrixChainProblem/withDynamicsPlanning.java
+++ DynamicsProgramming/src/MatrixChainProblem/withDynamicsPlanning.java
@@ -1,15 +1,40 @@
 package MatrixChainProblem;
 
 public class withDynamicsPlanning {
-
-	private static int recurMatrixChain(int i,int j,int[] p)   //最初始的矩阵连乘问题算法
-	{
-	    if(i == j) return 0;  //i == j，即只有一个矩阵，计算次数当然为零
-	    int min = recurMatrixChain(i,i,p) + recurMatrixChain(i+1,j,p) + p[i-1] * p[i] * p[j];
-	    for(int k = i + 1; k < j; k++){
-	        int t = recurMatrixChain(i,k,p) + recurMatrixChain(k+1,j,p) + p[i-1] * p[k] * p[j];
-	        if(t < min) min = t; //从k处断开，如果t比min更小，则说明存在更优的解决方法，把t赋值给min
-	    }
-	    return min;
+	static int n;
+	
+	
+	public static void main(String[] args) {
+		n = 6; //矩阵的个数(待确认
+		int l = n+1; //数组长度
+		int[][] m = new int[l][l];
+		int[][] s = new int[l][l];
+		int[] p = {30, 35, 15, 5, 10, 20, 25};
+		MatrixChain(p, m, s);
+		for (int i=0; i<=n; i++) {
+			for (int j=0; j<=n; j++)
+				System.out.print(m[i][j]+"   ");
+			System.out.println();
+		}
+		
+	}
+	
+	
+	public static void MatrixChain(int[] p, int[][] m, int[][]s) {
+		for (int i=1; i<=n; i++) m[i][i]=0;
+		
+		for (int r=2; r<=n; r++) 
+			for (int i=1; i<=n-r+1; i++) {
+				int j = i+r-1;
+				m[i][j] = m[i][i] + m[i+1][j] + p[i-1]*p[i]*p[j]; //设最先断开的位置是a
+				s[i][j] = i;
+				for (int k=i; k<j; k++) {
+					int t=m[i][k]+m[k+1][j]+p[i-1]*p[k]*p[j];
+					if (t<m[i][j]) {
+						m[i][j]=t;
+						s[i][j]=k;
+					}
+				}
+			}
 	}
 }
