9f7160560cff1aa84cfcf59a548bfc13a691a697
Wb-Alpha <45552873+Wb-Alpha@users.noreply.github.com>
2020-09-21 10:35:41 +0800

    new

--- Search/src/BinarySearchTree/BST.java
+++ Search/src/BinarySearchTree/BST.java
@@ -80,4 +80,63 @@ public class BST<Key extends Comparable<Key>, Value>{
 		if (t != null) return t;
 		else return x;
 	}
+	
+	public Key select(int k) {
+		return select(root, k).key;
+	}
+	
+	private Node select(Node x, int k) {
+		// 返回排名为k的节点
+		if (x == null) return null;
+		int t = size(x.left);
+		if (t > k) return select(x.left, k);
+		else if (t < k) return select(x.right, k);
+		else return x;
+	}
+	
+	public int rank(Key key) {
+		return rank(key, root);
+	}
+	
+	private int rank(Key key, Node x) {
+		// 返回以x为根节点的子树中小于x.key的键的数量
+		if (x == null) return 0;
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0) return rank(key, x.left);
+		else if (cmp > 0) return 1 + size(x.left) + size(x.right);
+		else return size(x.left);
+	}
+	
+	public void deleteMin() {
+		root = deleteMin(root);
+	}
+	
+	private Node deleteMin(Node x) {
+		if (x.left == null) return x.right;
+		x.left = deleteMin(x.left);
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+	
+	public void delete(Key key) {
+		root = delete(root, key);
+	}
+	
+	private Node delete(Node x, Key key) {
+		if (x == null) return null;
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0) x.left = delete(x.left, key);
+		else if (cmp > 0) x.right = delete(x.right, key);
+		else {
+			if (x.right == null) return x.left;
+			if (x.left == null) return x.right;
+			Node t = x;
+			x = min(t.right);
+			x.right = deleteMin(t.right);
+			x.left = t.left;
+		}
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+	
 }
