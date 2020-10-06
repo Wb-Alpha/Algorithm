a598081680cd55c2c4ad1cc2be83a5146b46afcf
Wb-Alpha <45552873+Wb-Alpha@users.noreply.github.com>
2020-10-06 23:45:12 +0800

    new

---
+++ 蓝桥真题/src/练习系统/节点选择.java
@@ -0,0 +1,33 @@
+package 练习系统;
+
+import java.util.Scanner;
+
+public class 节点选择 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[][] arr = new int[n][2];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i][0] = in.nextInt();
+		}
+		for (int i = 0; i < n-1; i++) {
+			for (int j = 0; j < 2; j++) {
+				
+			}
+		}
+	}
+	/*
+	class Node{
+		public int value;
+		public Node parent;
+		
+		public Node(int value) {
+			this.value = value;
+		}
+		
+		public void setParent(Node parent) {
+			this.parent = parent;
+		}
+	}*/
+}
